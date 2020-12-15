bbd461a6798947e08a0fe2f5e030acdc7faf4867
Lucas Fernandes <Lucas_201130@hotmail.com>
2020-12-14 22:51:54 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const dividir = require('./aritimetica/dividir.js');
+const somar = require('./aritimetica/somar.js');
+const multiplicar = require('./aritimetica/multiplicar.js');
+const subtrair = require('./aritimetica/subtrair.js');
+
+console.log(dividir(24,5))
+console.log(somar(24,5))
+console.log(multiplicar(24,5))
+console.log(subtrair(24,5))
